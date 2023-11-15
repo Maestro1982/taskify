86e210fec3f0d5226aa73f9b35de5abf2e22a405
Maestro1982 <kris.tote@telenet.be>
2023-11-15 15:33:38 +0100

    modify .gitignore, layout.tsx in app folder, create middleware.ts, layout.tsx in app platform folder, layout.tsx in app platform clerk folder, page.tsx in app platform clerk select-org [[...select-org]] folder, page.tsx in app platform clerk sign-in [[...sign-in]] folder, page.tsx in app platform clerk sign-up [[...sign-up]] folder, site.ts in config folder

---
+++ app/(platform)/layout.tsx
@@ -0,0 +1,7 @@
+import { ReactNode } from 'react';
+import { ClerkProvider } from '@clerk/nextjs';
+
+const PlatformLayout = ({ children }: { children: ReactNode }) => {
+  return <ClerkProvider>{children}</ClerkProvider>;
+};
+export default PlatformLayout;
