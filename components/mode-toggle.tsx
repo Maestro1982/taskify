b6ed62966fb4ecb579e36f46e9e532abc88b9014
Maestro1982 <kris.tote@telenet.be>
2023-11-16 11:15:45 +0100

    modify mode-toggle.tsx in components folder

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -18,7 +18,11 @@ export function ModeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant='outline' size='icon'>
+        <Button
+          variant='outline'
+          size='icon'
+          className='bg-opacity-80 backdrop-blur-[0.5rem] border border-none border-opacity-40 transition-all dark:bg-opacity-80 dark:backdrop-blur-[0.5rem] dark:border-opacity-40 dark:bg-[#292929] dark:hover:bg-black h-8 w-8'
+        >
           <Sun className='h-[1rem] w-[1rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
           <Moon className='absolute h-[1rem] w-[1rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
           <span className='sr-only'>Toggle theme</span>
