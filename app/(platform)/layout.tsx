94df94e4388f99aa8f08ae292b3ebf7885a9b6a2
Maestro1982 <kris.tote@telenet.be>
2023-11-17 11:18:08 +0100

    modify layout.tsx in app platform folder, page.tsx in app platform clerk select-org [[...select-org]] folder, page.tsx in app platform clerk sign-in [[...sign-in]] folder, page.tsx in app platform clerk sign-up [[...sign-up]] folder

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -1,7 +1,20 @@
+'use client';
+
 import { ReactNode } from 'react';
 import { ClerkProvider } from '@clerk/nextjs';
+import { dark } from '@clerk/themes';
+import { useTheme } from 'next-themes';
 
 const PlatformLayout = ({ children }: { children: ReactNode }) => {
-  return <ClerkProvider>{children}</ClerkProvider>;
+  const { theme } = useTheme();
+  return (
+    <ClerkProvider
+      appearance={{
+        baseTheme: theme === 'dark' ? dark : undefined,
+      }}
+    >
+      {children}
+    </ClerkProvider>
+  );
 };
 export default PlatformLayout;
