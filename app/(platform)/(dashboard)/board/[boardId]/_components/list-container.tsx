8910ef76a5f06d07f5c849ebc5a592c2f19ddabe
Maestro1982 <kris.tote@telenet.be>
2023-11-23 15:44:22 +0100

    modify page.tsx in app platform dashboard board boardId folder, schema.prisma in prisma folder, create types.ts and list-container.tsx in app platform dashboard board boardId _components folder

---
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import { ListWithCards } from '@/types';
+
+interface ListContainerProps {
+  data: ListWithCards[];
+  boardId: string;
+}
+
+export const ListContainer = ({ data, boardId }: ListContainerProps) => {
+  return <div>List Container</div>;
+};
